531e7cdb0d80f56ce18e4a92dbbd0ccbf42067db
Akash Biswas <111556960+akash-biswas-dev@users.noreply.github.com>
2025-06-26 23:03:44 +0530

    Update the changes.

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,6 +1,5 @@
 def call(String credentialId){
   withCredentials([usernamePassword(credentialsId: "${credentialId}", passwordVariable:"username" , usernameVariable:"password")]){
-    echo "${username} ${password}"
     sh "docker login -u ${env.username} -p ${env.password}"
   
   }
