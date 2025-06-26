49e6657ef792dd7ad61496662629e2e26f96fe13
Akash Biswas <111556960+akash-biswas-dev@users.noreply.github.com>
2025-06-26 23:09:46 +0530

    Fix the error in "dockerPush.groovy"

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,5 +1,5 @@
 def call(String credentialId){
-  withCredentials([usernamePassword(credentialsId: "${credentialId}", passwordVariable:"username" , usernameVariable:"password")]){
+  withCredentials([usernamePassword(credentialsId: "${credentialId}", passwordVariable:"password" , usernameVariable:"username")]){
     sh "docker login -u ${env.username} -p ${env.password}"
   
   }
