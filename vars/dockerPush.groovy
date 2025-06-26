242dde7cf015e600c16d985b9623359c55e7a5fa
Akash Biswas <111556960+akash-biswas-dev@users.noreply.github.com>
2025-06-26 22:34:15 +0530

    Create "dockerPush.groovy" to push images to docker-hub.

---
+++ vars/dockerPush.groovy
@@ -0,0 +1,6 @@
+def call(String credentialId){
+  withCredentials([usernamePassword(credentialsId: "${credentialId}", passwordVariable:"username" , usernameVariable:"password")]){
+    sh "docker login -u ${env.username}" -p "${env.password}"
+  
+  }
+}
